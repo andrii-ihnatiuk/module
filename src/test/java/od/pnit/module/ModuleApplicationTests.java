96c982f1bbc64ea6ef38f4c3d0ffaa05bed5c952
Andrew Ihnatuk <andreyignatuk2002@gmail.com>
2021-04-16 14:05:27 +0300

    first commit

---
+++ src/test/java/od/pnit/module/ModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package od.pnit.module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
